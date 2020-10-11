3afc31aa9e5fb27804ad782d8280f49c4ba80303
Maximilian Stölzle <maximilian@stoelzle.ch>
2020-10-11 15:27:00 +0200

    added system matrices for 3d state estimation

--- Src/kf.c
+++ Src/kf.c
@@ -1,63 +1,79 @@
 #include "../Inc/kf.h"
 
 void reset_kf_state(kf_state_t *kf_state){
-    if (STATE_ESTIMATION_FREQUENCY == 1000) {
-        float A_init[NUMBER_STATES][NUMBER_STATES] = {{1.0E-0, 1.0E-3, 5.0E-7}, {0, 1.0E-0, 1.0E-3}, {0.0, 0.0, 1.0E-0}};
-        float B_init[NUMBER_STATES][NUMBER_INPUTS] = {{5.0E-7}, {1.0E-3}, {0.0}};
-        float G_init[NUMBER_STATES][NUMBER_PROCESS_NOISE] = {{5.0E-7}, {1.0E-3}, {0.0}};
-        memcpy(kf_state->Ad, A_init, sizeof(kf_state->Ad));
-        memcpy(kf_state->Bd, B_init, sizeof(kf_state->Bd));
-        memcpy(kf_state->Gd, G_init, sizeof(kf_state->Gd));
-    } else if (STATE_ESTIMATION_FREQUENCY == 200) {
-        float A_init[NUMBER_STATES][NUMBER_STATES] = {{1.0E-0, 5.0E-3, 1.25E-5}, {0, 1.0E-0, 5.0E-3}, {0.0, 0.0, 1.0E-0}};
-        float B_init[NUMBER_STATES][NUMBER_INPUTS] = {{1.25E-5}, {5.0E-3}, {0.0}};
-        float G_init[NUMBER_STATES][NUMBER_PROCESS_NOISE] = {{1.25E-5}, {5.0E-3}, {0.0}};
-        memcpy(kf_state->Ad, A_init, sizeof(kf_state->Ad));
-        memcpy(kf_state->Bd, B_init, sizeof(kf_state->Bd));
-        memcpy(kf_state->Gd, G_init, sizeof(kf_state->Gd));
-    } else if (STATE_ESTIMATION_FREQUENCY == 100) { 
-        float A_init[NUMBER_STATES][NUMBER_STATES] = {{1.0E-0, 1.0E-2, 5.0E-5}, {0, 1.0E-0, 1.0E-2}, {0.0, 0.0, 1.0E-0}};
-        float B_init[NUMBER_STATES][NUMBER_INPUTS] = {{5.0E-5}, {1.0E-2}, {0.0}};
-        float G_init[NUMBER_STATES][NUMBER_PROCESS_NOISE] = {{5.0E-5}, {1.0E-2}, {0.0}};
-        memcpy(kf_state->Ad, A_init, sizeof(kf_state->Ad));
-        memcpy(kf_state->Bd, B_init, sizeof(kf_state->Bd));
-        memcpy(kf_state->Gd, G_init, sizeof(kf_state->Gd));
-    } else if (STATE_ESTIMATION_FREQUENCY == 50) { 
-        float A_init[NUMBER_STATES][NUMBER_STATES] = {{1.0E-0, 2.0E-2, 2.0E-4}, {0, 1.0E-0, 2.0E-2}, {0.0, 0.0, 1.0E-0}};
-        float B_init[NUMBER_STATES][NUMBER_INPUTS] = {{2.0E-4}, {2.0E-2}, {0.0}};
-        float G_init[NUMBER_STATES][NUMBER_PROCESS_NOISE] = {{2.0E-4}, {2.0E-2}, {0.0}};
-        memcpy(kf_state->Ad, A_init, sizeof(kf_state->Ad));
-        memcpy(kf_state->Bd, B_init, sizeof(kf_state->Bd));
-        memcpy(kf_state->Gd, G_init, sizeof(kf_state->Gd));
-    } else if (STATE_ESTIMATION_FREQUENCY == 40) { 
-        float A_init[NUMBER_STATES][NUMBER_STATES] = {{1.0E-0, 2.5E-2, 3.125E-4}, {0, 1.0E-0, 2.5E-2}, {0.0, 0.0, 1.0E-0}};
-        float B_init[NUMBER_STATES][NUMBER_INPUTS] = {{3.125E-4}, {2.5E-2}, {0.0}};
-        float G_init[NUMBER_STATES][NUMBER_PROCESS_NOISE] = {{3.125E-4}, {2.5E-2}, {0.0}};
-        memcpy(kf_state->Ad, A_init, sizeof(kf_state->Ad));
-        memcpy(kf_state->Bd, B_init, sizeof(kf_state->Bd));
-        memcpy(kf_state->Gd, G_init, sizeof(kf_state->Gd));
-    } else if (STATE_ESTIMATION_FREQUENCY == 33.33) { 
-        float A_init[NUMBER_STATES][NUMBER_STATES] = {{1.0E-0, 3.003E-2, 4.5009E-4}, {0, 1.0E-0, 3.003E-2}, {0.0, 0.0, 1.0E-0}};
-        float B_init[NUMBER_STATES][NUMBER_INPUTS] = {{4.5009E-4}, {3.003E-2}, {0.0}};
-        float G_init[NUMBER_STATES][NUMBER_PROCESS_NOISE] = {{4.5009E-4}, {3.003E-2}, {0.0}};
-        memcpy(kf_state->Ad, A_init, sizeof(kf_state->Ad));
-        memcpy(kf_state->Bd, B_init, sizeof(kf_state->Bd));
-        memcpy(kf_state->Gd, G_init, sizeof(kf_state->Gd));
-    } else if (STATE_ESTIMATION_FREQUENCY == 1) { 
-        float A_init[NUMBER_STATES][NUMBER_STATES] = {{1.0E-0, 1.0E-0, 5.0E-1}, {0, 1.0E-0, 1.0E-0}, {0.0, 0.0, 1.0E-0}};
-        float B_init[NUMBER_STATES][NUMBER_INPUTS] = {{5.0E-1}, {1.0E-0}, {0.0}};
-        float G_init[NUMBER_STATES][NUMBER_PROCESS_NOISE] = {{5.0E-1}, {1.0E-0}, {0.0}};
-        memcpy(kf_state->Ad, A_init, sizeof(kf_state->Ad));
-        memcpy(kf_state->Bd, B_init, sizeof(kf_state->Bd));
-        memcpy(kf_state->Gd, G_init, sizeof(kf_state->Gd));
-    } else {
-        float A[NUMBER_STATES][NUMBER_STATES] = {{0, 1, 0}, {0, 0, 1}, {0, 0, 0}};
-        float B[NUMBER_STATES][NUMBER_INPUTS] = {{0}, {1}, {0}};
-        float G[NUMBER_STATES][NUMBER_INPUTS] = {{0}, {1}, {0}};
+    #if STATE_ESTIMATION_TYPE == 1
+        if (STATE_ESTIMATION_FREQUENCY == 1000) {
+            float A_init[NUMBER_STATES][NUMBER_STATES] = {{1.0E-0, 1.0E-3, 5.0E-7}, {0, 1.0E-0, 1.0E-3}, {0.0, 0.0, 1.0E-0}};
+            float B_init[NUMBER_STATES][NUMBER_INPUTS] = {{5.0E-7}, {1.0E-3}, {0.0}};
+            float G_init[NUMBER_STATES][NUMBER_PROCESS_NOISE] = {{5.0E-7}, {1.0E-3}, {0.0}};
+            memcpy(kf_state->Ad, A_init, sizeof(kf_state->Ad));
+            memcpy(kf_state->Bd, B_init, sizeof(kf_state->Bd));
+            memcpy(kf_state->Gd, G_init, sizeof(kf_state->Gd));
+        } else if (STATE_ESTIMATION_FREQUENCY == 200) {
+            float A_init[NUMBER_STATES][NUMBER_STATES] = {{1.0E-0, 5.0E-3, 1.25E-5}, {0, 1.0E-0, 5.0E-3}, {0.0, 0.0, 1.0E-0}};
+            float B_init[NUMBER_STATES][NUMBER_INPUTS] = {{1.25E-5}, {5.0E-3}, {0.0}};
+            float G_init[NUMBER_STATES][NUMBER_PROCESS_NOISE] = {{1.25E-5}, {5.0E-3}, {0.0}};
+            memcpy(kf_state->Ad, A_init, sizeof(kf_state->Ad));
+            memcpy(kf_state->Bd, B_init, sizeof(kf_state->Bd));
+            memcpy(kf_state->Gd, G_init, sizeof(kf_state->Gd));
+        } else if (STATE_ESTIMATION_FREQUENCY == 100) { 
+            float A_init[NUMBER_STATES][NUMBER_STATES] = {{1.0E-0, 1.0E-2, 5.0E-5}, {0, 1.0E-0, 1.0E-2}, {0.0, 0.0, 1.0E-0}};
+            float B_init[NUMBER_STATES][NUMBER_INPUTS] = {{5.0E-5}, {1.0E-2}, {0.0}};
+            float G_init[NUMBER_STATES][NUMBER_PROCESS_NOISE] = {{5.0E-5}, {1.0E-2}, {0.0}};
+            memcpy(kf_state->Ad, A_init, sizeof(kf_state->Ad));
+            memcpy(kf_state->Bd, B_init, sizeof(kf_state->Bd));
+            memcpy(kf_state->Gd, G_init, sizeof(kf_state->Gd));
+        } else if (STATE_ESTIMATION_FREQUENCY == 50) { 
+            float A_init[NUMBER_STATES][NUMBER_STATES] = {{1.0E-0, 2.0E-2, 2.0E-4}, {0, 1.0E-0, 2.0E-2}, {0.0, 0.0, 1.0E-0}};
+            float B_init[NUMBER_STATES][NUMBER_INPUTS] = {{2.0E-4}, {2.0E-2}, {0.0}};
+            float G_init[NUMBER_STATES][NUMBER_PROCESS_NOISE] = {{2.0E-4}, {2.0E-2}, {0.0}};
+            memcpy(kf_state->Ad, A_init, sizeof(kf_state->Ad));
+            memcpy(kf_state->Bd, B_init, sizeof(kf_state->Bd));
+            memcpy(kf_state->Gd, G_init, sizeof(kf_state->Gd));
+        } else if (STATE_ESTIMATION_FREQUENCY == 40) { 
+            float A_init[NUMBER_STATES][NUMBER_STATES] = {{1.0E-0, 2.5E-2, 3.125E-4}, {0, 1.0E-0, 2.5E-2}, {0.0, 0.0, 1.0E-0}};
+            float B_init[NUMBER_STATES][NUMBER_INPUTS] = {{3.125E-4}, {2.5E-2}, {0.0}};
+            float G_init[NUMBER_STATES][NUMBER_PROCESS_NOISE] = {{3.125E-4}, {2.5E-2}, {0.0}};
+            memcpy(kf_state->Ad, A_init, sizeof(kf_state->Ad));
+            memcpy(kf_state->Bd, B_init, sizeof(kf_state->Bd));
+            memcpy(kf_state->Gd, G_init, sizeof(kf_state->Gd));
+        } else if (STATE_ESTIMATION_FREQUENCY == 33.33) { 
+            float A_init[NUMBER_STATES][NUMBER_STATES] = {{1.0E-0, 3.003E-2, 4.5009E-4}, {0, 1.0E-0, 3.003E-2}, {0.0, 0.0, 1.0E-0}};
+            float B_init[NUMBER_STATES][NUMBER_INPUTS] = {{4.5009E-4}, {3.003E-2}, {0.0}};
+            float G_init[NUMBER_STATES][NUMBER_PROCESS_NOISE] = {{4.5009E-4}, {3.003E-2}, {0.0}};
+            memcpy(kf_state->Ad, A_init, sizeof(kf_state->Ad));
+            memcpy(kf_state->Bd, B_init, sizeof(kf_state->Bd));
+            memcpy(kf_state->Gd, G_init, sizeof(kf_state->Gd));
+        } else if (STATE_ESTIMATION_FREQUENCY == 1) { 
+            float A_init[NUMBER_STATES][NUMBER_STATES] = {{1.0E-0, 1.0E-0, 5.0E-1}, {0, 1.0E-0, 1.0E-0}, {0.0, 0.0, 1.0E-0}};
+            float B_init[NUMBER_STATES][NUMBER_INPUTS] = {{5.0E-1}, {1.0E-0}, {0.0}};
+            float G_init[NUMBER_STATES][NUMBER_PROCESS_NOISE] = {{5.0E-1}, {1.0E-0}, {0.0}};
+            memcpy(kf_state->Ad, A_init, sizeof(kf_state->Ad));
+            memcpy(kf_state->Bd, B_init, sizeof(kf_state->Bd));
+            memcpy(kf_state->Gd, G_init, sizeof(kf_state->Gd));
+        } else {
+            float A[NUMBER_STATES][NUMBER_STATES] = {{0, 1, 0}, {0, 0, 1}, {0, 0, 0}};
+            float B[NUMBER_STATES][NUMBER_INPUTS] = {{0}, {1}, {0}};
+            float G[NUMBER_STATES][NUMBER_INPUTS] = {{0}, {1}, {0}};
+
+            discretize(STATE_ESTIMATION_FREQUENCY, NUMBER_STATES, NUMBER_INPUTS, A, B, kf_state->Ad, kf_state->Bd);
+            discretize(STATE_ESTIMATION_FREQUENCY, NUMBER_STATES, NUMBER_INPUTS, A, G, kf_state->Ad, kf_state->Gd);
+        }
+    #elif STATE_ESTIMATION_TYPE == 2
+        float A[NUMBER_STATES][NUMBER_STATES] = {0};
+        float B[NUMBER_STATES][NUMBER_INPUTS] = {0};
+        float G[NUMBER_STATES][NUMBER_INPUTS] = {0};
+
+        for(int i = 0; i < 3; i++) {
+            A[i][2+i] = 1;
+        }
+        for(int i = 0; i < 6; i++) {
+            B[2+i][i] = 1;
+        }
 
         discretize(STATE_ESTIMATION_FREQUENCY, NUMBER_STATES, NUMBER_INPUTS, A, B, kf_state->Ad, kf_state->Bd);
-        discretize(STATE_ESTIMATION_FREQUENCY, NUMBER_STATES, NUMBER_INPUTS, A, G, kf_state->Ad, kf_state->Gd);
-    }
+        memcpy(&kf_state->Gd, &kf_state->Bd, sizeof(kf_state->Bd));
+    #endif
 
 	float x_est_init[NUMBER_STATES] = {0};
 	float P_est_init[NUMBER_STATES][NUMBER_STATES] = {{1.0E-9, 0, 0}, {0, 1.0E-12, 0}, {0, 0, 0}};
